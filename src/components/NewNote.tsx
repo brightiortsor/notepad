31f38671828a44bac552562d30121ec2f16adfc9
Bright Iortsor <iortsor@gmail.com>
2023-02-08 17:40:19 +0100

    added Notes Form area

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -1,3 +1,10 @@
+import { NoteForm } from "./NoteForm";
+
 export const NewNote = () => {
-  return <div>NewNote</div>;
+  return (
+    <div>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </div>
+  );
 };
---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,29 @@
+import { Col, Form, Row, Stack } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
+
+export const NoteForm = () => {
+  return (
+    <Form>
+      <Stack gap={3}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Enter title" required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control as="textarea" rows={10} required />
+        </Form.Group>
+      </Stack>
+    </Form>
+  );
+};
