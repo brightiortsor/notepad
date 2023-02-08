9a5f828287d6a2696e87cc1037949e918b865053
Bright Iortsor <iortsor@gmail.com>
2023-02-08 15:50:32 +0100

    added note paa dependencies

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./components/NewNote";
+function App() {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show Task</h1>} />
+          <Route path="edit" element={<h1>Edit task</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/NewNote.tsx
@@ -0,0 +1,3 @@
+export const NewNote = () => {
+  return <div>NewNote</div>;
+};
